0ea4b9c29f06ab5df2bc27547f61150643d69812
cassyblaise <cassyblaise@gmail.com>
2021-06-14 19:34:44 +0100

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,81 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  siteMetadata: {
+    title: `cassypost`,
+    description: `A Gatsby Netlify CMS powered Blog Starter to boost your development.`,
+    author: `@cassyjnr`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-sharp`,
+    `gatsby-transformer-json`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-catch-links`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/images`,
+        name: `images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./content/data`,
+        name: `topic`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/articles`,
+        name: `articles`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+          endpoint: 'https://gmail.us20.list-manage.com/subscribe/post?u=bf58177dc8a1774b00194239c&amp;id=7be02f6ae4'
+          // timeout: 3500,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-relative-images`,
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#f7102b`,
+        display: `minimal-ui`,
+        icon: `static/favicon.ico`,
+      },
+    },
+
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/components/mailchimpform.js
@@ -0,0 +1,64 @@
+import React from "react"
+import addToMailchimp from 'gatsby-plugin-mailchimp'
+
+export default class MailChimpForm extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = { 
+          email: "", 
+          message: "Subscribe to my list for lots of great reasons"
+        }
+    }
+    changeEmailHandler = (event) => {
+        this.setState({email: event.target.value})
+    }
+
+    handleSubmit = e => {
+      e.preventDefault();
+      this.setState({message:'Thanks for subscribing'})
+      addToMailchimp(this.state.email) // listFields are optional if you are only capturing the email address.
+      .then(data => {
+        // I recommend setting data to React state
+        // but you can do whatever you want (including ignoring this `then()` altogether)
+        console.log(data)
+      })
+      .catch(() => {
+        // unnecessary because Mailchimp only ever
+        // returns a 200 status code
+        // see below for how to handle errors
+      })
+      this.setState({email: ''})
+    }
+
+  //   handleSubmit = e => {
+  //     e.preventDefault()
+  //   //const result = await addToMailchimp(this.state.email)
+  //     this.setState({message: 'Thanks for subcribing'})
+  //     addToMailchimp(this.state.email)
+  //     console.log(e)
+      
+  //   }
+  // handleChange = event => {
+  //     this.setState({ email: event.target.value })
+  //   }
+
+  render() {
+      return (
+
+        <div className="sidebar-emails">
+              <h2>Mailing list here</h2>
+              <p>Subscribe to my list for lots of great reasons</p>
+              <form onSubmit={this.handleSubmit}> 
+                <input type="text"  value={this.state.email}  onChange={this.changeEmailHandler} id="email" aria-label="email" />
+                <input
+                  type="submit"
+                  value="Subscribe"
+                  aria-label="subscribe"
+                />{" "}
+              </form>
+              <span>Weekly updates, unsubscribe at any time</span>
+            </div>
+       
+      )
+    }
+  }
